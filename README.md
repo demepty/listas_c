029dbfdb3ce27698e7a4543cb82c727654b95587
Demetrio Gonzalez <demetrio.gonzalez@up.ac.pa>
2019-06-07 09:17:40 -0600

    Adaptacion del programa para polinomios
    Se adapto el programa de listas para que pueda manejar polinomios.
    
     Changes to be committed:
    	modified:   README.md
    	modified:   listas.cpp

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# listas_c
-Listas enlazadas en c++
+#v1 Listas enlazadas en c++
 Una lista es una estructura dinámica de datos. Cada objeto de la estructura está formado por los datos junto con un puntero al siguiente objeto. Al manejar punteros, los datos no tienen por qué estar situados en posiciones consecutivas de la memoria, y lo más normal, es que estén dispersos.
+
+#v2 Polinomios con listas enlazadas
--- listas.cpp
+++ listas.cpp
@@ -5,6 +5,7 @@
 
 struct nodo{
 	int dato;
+	int exp;
 	struct nodo * sgt;
 };
 
@@ -13,6 +14,8 @@ typedef struct nodo*Tlista;
 void agregar(Tlista & );
 void mostrar(Tlista &);
 int calcular(Tlista &);
+void sumar(Tlista &, Tlista &, Tlista &);
+void poli(int sum, int expo ,Tlista &);
 void menu(int op);
 
 Tlista lista1=NULL;
@@ -29,12 +32,12 @@ do{
 	cout <<"1) Cargar lista "<<endl;	
 	cout <<"3) Imprimir lista"<<endl;	
 	cout <<"4) Calcular el tamaño de la lista"<<endl;	
+	cout <<"6) sumar"<<endl;	
 	cout<<"5) Para salir"<<endl;	
 	cin>>opc;
 	menu(opc);
 	system("pause");
 }while(opc!=5);
-
 
  return 0;
 }
@@ -56,7 +59,7 @@ void menu(int opcion){
 	else
 	{
 		agregar(lista2);
-	}
+	}	
 	break;
 	case 3: 
 	cout<<" Ingrese el numero para la lista "<<endl;
@@ -67,11 +70,16 @@ void menu(int opcion){
 	{
 		mostrar(lista1);
 	}
+	else if(dato==2){
+		mostrar(lista2);
+	}
 	else
 	{
-		mostrar(lista2);
+		mostrar(lista3);
 	}	
-	break;	
+	break;
+	case 6: sumar(lista1,lista2,lista3); break;
+	
 	case 4:
 	cout<<" Ingrese el numero para la lista "<<endl;
 	cout<<" 1"<<endl;
@@ -84,7 +92,7 @@ void menu(int opcion){
 	}
 	else
 	{
-		res= calcular(lista2);
+		res= calcular(lista3);
 		cout<<"res"<<res;
 	}	
 	break;		
@@ -95,32 +103,37 @@ void menu(int opcion){
 
 void agregar(Tlista &lista){
 int ndato;
+int exp;
 Tlista p;
 Tlista q;
 Tlista t;
 do{
 system("cls");
-cout<<" Ingrese el numero para la lista, '0' para salir ";
+cout<<" Ingrese el coeficiente o '0' para salir::";
 cin>>ndato;
+cout<<" Ingrese el exponente :";
+cin>>exp;
 if(ndato==0){
 	cout<<" No se agregan mas elementos a la lista..\n "<<endl;;
 }
 else{
 		p= new nodo();	
 		p->dato=ndato;
+		p->exp=exp;
 		p->sgt=lista;
 		
 	if(lista==NULL){	
 		
 		lista=p;
-		cout<<" elemento agregado correctamente"<<endl;;
+		cout<<" elemento agregado correctamente"<<endl;;		
 		system("pause");		
 	}
 	else
 	{
-		q=lista;
+		cout<<" ya tenia datos"<<endl;;
+		q=lista;//->1
 		
-		while((q!=NULL)&&(p->dato <= q->dato)){
+		while((q!=NULL)&&(p->exp <= q->exp)){
 			t=q;
 			q=q->sgt;				
 		}
@@ -131,43 +144,133 @@ else{
 		else{
 			t->sgt=p;
 		}
-		p->sgt=q;
-	
-	}
-	
+		p->sgt=q;	
+	}	
 }
 
 }while(ndato!=0);
 
 }
 
+
+
 void mostrar(Tlista &lista){
 	
-cout<<" Imprimir lista "<<endl;
-nodo *lis_aux;
-lis_aux=lista;
-int i=0;
-
-while(lis_aux != NULL)
-{
+	cout<<" Imprimir lista "<<endl;
+	nodo *lis_aux;
+	lis_aux=lista;
+	int i=0;
 	
-	cout<<lis_aux->dato<<" -->";	
-	lis_aux=lis_aux->sgt;
-	i++;
-}
+	while(lis_aux != NULL)
+	{	
+		cout<<lis_aux->dato<<"x^"<<lis_aux->exp<<" ";	
+		lis_aux=lis_aux->sgt;
+		i++;
+	}
 
 }
 
 int calcular( Tlista &lista){
-nodo *l;
-int n=0;
-l=lista;
-while(l!= NULL){
-n++;	
-l=l->sgt;
-cout<<"n"<<n;
+	nodo *l;
+	int n=0;
+	l=lista;
+	while(l!= NULL){
+	n++;	
+	l=l->sgt;
+	cout<<"n"<<n;
+	}
+	return n;
+
 }
-return n;
+
+void sumar(Tlista &lista1,Tlista &lista2,Tlista &lista3){
+	
+	nodo *lis_aux1,*lis_aux2,*lis_aux3;
+	lis_aux1=lista1;
+	lis_aux2=lista2;
+	lis_aux3=lista3;
+	int suma;
+	
+	while(lis_aux1!=NULL && lis_aux2!=NULL)
+      {
+           if(lis_aux1->exp==lis_aux2->exp)
+           {
+               suma=lis_aux1->dato+lis_aux2->dato;
+               if(suma!=0)
+               {                    
+                  	poli(suma,lis_aux1->exp,lis_aux3);
+                    lis_aux1=lis_aux1->sgt;
+                    lis_aux2=lis_aux2->sgt;
+               }
+           }
+           else
+           {
+                if(lis_aux1->exp > lis_aux2->exp)
+                {                
+                   poli(lis_aux1->dato,lis_aux1->exp,lis_aux3);
+                   lis_aux1=lis_aux1->sgt;
+                }
+                else
+                {                 
+                poli(lis_aux2->dato,lis_aux2->exp,lis_aux3);
+                lis_aux2=lis_aux2->sgt;                   
+                }
+           }
+      }
+      while(lis_aux1!=NULL)
+      {
+       
+          poli(lis_aux3->dato,lis_aux1->exp,lis_aux3);
+          lis_aux1=lis_aux1->sgt;
+      }
+      while(lis_aux2!=NULL)
+      {
+       
+         poli(lis_aux2->dato,lis_aux2->exp,lis_aux3);
+          lis_aux2=lis_aux2->sgt;
+      }
+      //cout<<"P(x)+Q(x)= ";f->recorre(f);
+	
+	 
+}
+
+void poli(int sum,int expo,Tlista &lista){
+int ndato=sum;
+int exp=expo;
+Tlista p;
+Tlista q;
+Tlista t;
+
+		p= new nodo();	
+		p->dato=ndato;
+		p->exp=exp;
+		p->sgt=lista;
+		
+	if(lista==NULL){	
+		
+		lista=p;
+		//cout<<" elemento agregado correctamente"<<endl;;
+		//system("pause");		
+	}
+	else
+	{
+		cout<<" no esta null"<<endl;;
+		q=lista;//->1
+		
+		while((q!=NULL)&&(p->exp <= q->exp)){
+			t=q;
+			q=q->sgt;				
+		}
+		if(lista==q){
+			p->sgt=q;
+			lista=p;
+		}	
+		else{
+			t->sgt=p;
+		}
+		p->sgt=q;	
+	}	
+
 
 }
 
